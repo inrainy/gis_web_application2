159caeb23e6d368b9c6be01375b8637abf535aa6
inrainy <86110182+inrainy@users.noreply.github.com>
2021-07-09 10:49:01 +0900

    7.9.webapplication2.4

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def hi_world(request):
-    if request.method =='POST':
+    if request.method == "POST":
         return render(request, 'accountapp/hi_world.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'text' : 'POST METHOD!'})
     else:
         return render(request, 'accountapp/hi_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'text' : 'GET METHOD!'})
+
